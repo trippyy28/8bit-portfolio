0712fd617cb9a1beb334ab6fbe770858f819a261
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2021-03-03 11:38:13 +0200

    adding email emailjs service

--- src/Container.js
+++ src/Container.js
@@ -1,16 +1,37 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Container = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [feedback, setFeedback] = useState("");
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ku8xy6k",
+        "template_zxwhczq",
+        e.target,
+        "user_doQIk315T5vjvtjFnz3je"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
   return (
     <div>
       <div className="container nes-container with-title mb-4">
         <p className="title">Contact Me</p>
         <div className="row">
           <div className="col md-12">
-            <form action="" className="form-horizontal">
+            <form action="" className="form-horizontal" onSubmit={sendEmail}>
               <div className="nes-balloon from-left">
                 <legend className="text-center mb-4">
                   Send Message or link on social media
